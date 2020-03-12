0d71a5b5cbbade755e15a66dfcd42684a3850119
pleum187156 <62104207+pleum187156@users.noreply.github.com>
2020-03-12 20:52:33 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,33 @@
+function getParams() {
+  	var idx = document.URL.indexOf('?');
+  	var params = new Array();
+  	if (idx != -1) {
+  		var pairs = document.URL.substring(idx+1, document.URL.length).split('&');
+  		for (var i=0; i<pairs.length; i++) {
+  			nameVal = pairs[i].split('=');
+  			params[nameVal[0]] = nameVal[1];
+  		}
+ 	}
+  	return params;
+ }
+
+window.onload = loginLoad;
+function loginLoad(){
+	// วิธีใช้ getParams()
+	loginform = document.getElementById("myLogin");
+	loginform.onsubmit = checkLogin;
+ parameter = getParams();
+	
+}
+
+function checkLogin(){
+	//ถ้าตรวจสอบแล้วพบว่ามีการ login ไม่ถูกต้อง ให้ return false ด้วย
+	if(loginform.username.value == parameter["username"] && loginform.password.value == parameter["password"]){
+	alert("Login Complete.")
+}
+else {
+	alert("Wrong Username or Password");
+	return false;
+	}
+}
+			
